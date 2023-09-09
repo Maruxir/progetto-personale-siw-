2c1be0b1e5ac50d25343e15e1b5f389f25c6d869
Maruxir <irene.marucci00@gmail.com>
2023-09-09 18:46:50 +0200

    creazione progetto

---
+++ ProgettoPersonaleSiw/src/main/java/com/prenotazioni/demo/ProgettoPersonaleSiwApplication.java
@@ -0,0 +1,13 @@
+package com.prenotazioni.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPersonaleSiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoPersonaleSiwApplication.class, args);
+	}
+
+}
---
+++ ProgettoPersonaleSiw/src/test/java/com/prenotazioni/demo/ProgettoPersonaleSiwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prenotazioni.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoPersonaleSiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
